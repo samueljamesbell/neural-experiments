8c793a51fb79996458165a54409c8a5bb81d77bc
Samuel Bell <samueljamesbell@gmail.com>
2018-02-13 01:03:42 +0000

    Factor gradient descent out of backprop

--- feed_forward_network.py
+++ feed_forward_network.py
@@ -17,9 +17,7 @@ _LEARNING_RATE = 0.0001
 
 class FeedForwardNet(object):
 
-    # TODO: Backprop could take these and return a list of error layer vectors.
-    # Gradient descent takes this, and actually updates weights and biases.§
-
+    # TODO: Generalise to flexible number of layers and dimensions.
     # TODO: Use softmax output layer and cross-entropy loss function.
 
     def __init__(self, number_training_examples):
@@ -44,11 +42,11 @@ class FeedForwardNet(object):
 
         for i in range(0, _TRAINING_EPOCHS):
             Z, A = self._forward_pass(X)
-            # TODO: Restore softmax output layer.
             # softmax_outputs = utils.softmax(self.A_o)
             # cost = cost.quadratic_cost_prime(softmax_outputs, Y_probabilities)
             C = cost.quadratic_cost_prime(A[-1], Y_probabilities)
-            self._back_propagate(C, X, Z, A)
+            deltas = self._back_propagate(C, X, Z, A)
+            self._batch_gradient_descent(deltas, X, A)
 
         # Perform a final forward pass with our optimised weights.
         _, A = self._forward_pass(X)
@@ -94,7 +92,7 @@ class FeedForwardNet(object):
         return Z, A
 
     def _back_propagate(self, C, X, Z, A):
-        """Update weights and biases according to the cost, C.
+        """Calculate error w.r.t C for each layer.
 
         C is an o x n matrix, where o is the dimensionality of the output
         layer and n is the number of training examples. C represents a matrix
@@ -114,24 +112,41 @@ class FeedForwardNet(object):
         The final item in A is the output layer, and the first item is the
         first hidden layer. The same is true for Z.
 
-        Uses backpropagation to apportion cost to each parameter.
-
-        Currently also performs batch (that's right, the whole batch) gradient
-        descent to actually update the parameters. We should probably just
-        calculate the error of each layer here.
+        Returns deltas, a list of errors where each entry in the list is a
+        1 x d matrix where d is the dimensionality of the layer, representing
+        the error w.r.t each activation in the layer.
         """
-        output_layer_error = np.multiply(
-            C, activation.sigmoid_prime(Z[-1]))
-        hidden_layer_error = np.multiply(self.W_h_o.T.dot(
-            output_layer_error), activation.sigmoid_prime(Z[-2]))
+        deltas = []
 
-        output_layer_bias_gradients = output_layer_error
-        hidden_layer_bias_gradients = hidden_layer_error
+        deltas.append(np.multiply(C, activation.sigmoid_prime(Z[-1])))
+        deltas.append(np.multiply(self.W_h_o.T.dot(
+            deltas[0]), activation.sigmoid_prime(Z[-2])))
 
-        output_layer_weight_gradients = output_layer_error.dot(A[-2].T)
-        hidden_layer_weight_gradients = hidden_layer_error.dot(X.T)
+        # We reverse the order of deltas so its indices align with A.
+        # That is, so that the error delta at index i is the error of the A[i].
+        return deltas[::-1]
 
-        # TODO: This is gradient descent - should probably be its own method
+    def _batch_gradient_descent(self, deltas, X, A):
+        """Update weights according to the layer-by-layer errors.
+
+        deltas is a list of errors where each entry in the list is a
+        1 x d matrix where d is the dimensionality of the layer, representing
+        the error w.r.t each activation in the layer.
+
+        X is an f x n matrix, where f is the number of features and n is the
+        number of training examples.
+
+        A is a list of activations, where each entry is a d x n matrix, where
+        d is the dimensionality of the layer and n is the number of data points.
+        
+        Currently performs batch (that's right, the whole batch) gradient
+        descent to actually update the parameters.
+        """
+        output_layer_bias_gradients = deltas[-1]
+        hidden_layer_bias_gradients = deltas[-2]
+
+        output_layer_weight_gradients = deltas[-1].dot(A[-2].T)
+        hidden_layer_weight_gradients = deltas[-2].dot(X.T)
 
         self.b_o = self.b_o - (np.mean(output_layer_bias_gradients,
                                        axis=1).reshape(self.b_o.shape[0], 1) *
